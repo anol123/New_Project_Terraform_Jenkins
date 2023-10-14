1135dca6e374344ca306574310a1b3936519bb45
M Rohit Singha <42908618+Rohit-01@users.noreply.github.com>
2023-10-14 23:31:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ terraform {
    required_version = ">= 1.2.0"
 }
 provider "aws" {
-    access_key = "ASIAR5BGBJPSYVDPNW7B"
-    secret_key = "wBAknYEb2ersyOr1FrlslJ8jWCVsRS8PHLycDI6A"
+    access_key = "ASIAR5BGBJPSZDQB4DGG"
+    secret_key = "CzcrufklRfPHCFi+vBrVmF/xg+RfPnYOYktTU6/P"
     region = "ap-south-1"
 }
 
@@ -17,6 +17,6 @@ resource "aws_instance" "foo" {
   ami           = "ami-0f5ee92e2d63afc18" # ap-south-1
   instance_type = "t2.micro"
   tags = {
-      Name = "TF-Instance"
+      Name = "Sonarqube"
   }
 }
